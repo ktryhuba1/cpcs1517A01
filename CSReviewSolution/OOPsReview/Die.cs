31c04612fcd8ba410511c26589f5ece0a7fcb61d
ktryhuba1 <46577134+ktryhuba1@users.noreply.github.com>
2019-01-16 14:50:51 -0700

    Update Die.cs

--- CSReviewSolution/OOPsReview/Die.cs
+++ CSReviewSolution/OOPsReview/Die.cs
@@ -8,13 +8,21 @@ namespace OOPsReview
 {
     public class Die
     {
+        //use the System.Random class for my RNG
+        //the instance will be static so all intances of Die will use the same generator
+        // the instance of rdm will be created when the first instance of Die is created
+        // and be destroyed when the last instance is destroyed
+
+        public static Random _rnd = new Random();
+
+
         //Data Member
         // this is the physical storage area for data values
         // these are usually private
         
         private int _Sides;
         private string _Color;
-        private int _FaceValue;
+       // private int _FaceValue;
 
 
         //Properties 
@@ -47,6 +55,7 @@ namespace OOPsReview
                 if (value >= 6 && value <= 20)
                 {
                     _Sides = value;
+                    Roll();
                 }
                 else
                 {
@@ -59,6 +68,87 @@ namespace OOPsReview
 
 
         //Auto Implimented Property
+        //there is no defined data member for this type of property
+        //instead, the system creates an internal storage area of the 
+        //  property datatype and manages the area for your code. 
+        // typically this type of property is commomly used when no 
+        // validation is required for the data
+        public int FaceValue { get; private set; }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                //need to check for null or just spaces
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                   throw new Exception("Die cannot have " + value.ToString() + "as a color. need a valid Color.");
+                }
+                else
+                {
+                     _Color = value;
+                }
+            }
+        }
+
+
+        //Constructors
+        // optional
+        // if you do not include a constructor for your class
+        // then when an instance of the class is created the system
+        // will assign a normal default values of that datatype to the appropriate data member
+
+        //if you declare an constructor in your class definition then you are responsible
+        // for all the constuctors for the class
+
+        //constructors are not called directly by the user of the class
+        // the constructor will be called when you ask the system to create an instance of the class
+
+        //syntax public classname([list of perameters]) { code body }
+
+        //default constructor
+        //this is similar to having a system constructor
+        //it has no perameters
+
+        public Die()
+        {
+            //typically this constructor will have no code, that is use the system defaults
+
+            //you could assign your own defaults
+
+            _Sides = 6;
+            Color = "White";
+            Roll();
+        }
+
+
+
+        //greedy constructor
+        //takes in values for each of your datamembers/auto-properties
+        //this allows the outside "user" wants to specify values at time of creation of the instance
+        public Die(int sides, string color)
+        {
+
+            Sides = sides;
+            Color = color;
+            Roll();
+
+        }
+        //behaviours (methods)
+        //a method will allow the user to 
+        // a) manupulate the data of the instance
+        // b) use the data of the instance to generate some other info
+
+
+        public void Roll()
+        {
+            //will generate a random number for facevalue
+            FaceValue = _rnd.Next(1, Sides + 1);
+        }
 
 
     }
